73f8e03dc6ae1b18abcdf9aca0ccab9bcc1a22a6
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2024-05-06 22:35:40 -0300

    Implementando Conteudo, Mentoria, Curso

---
+++ src/entities/Curso.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class Curso extends Conteudo{
+	private Integer cargaHoraria;
+	
+	public Curso(String titulo, String descricao, Integer cargaHoraria) {
+		super(titulo,descricao);
+		this.cargaHoraria = cargaHoraria;
+	}
+
+	public Integer getCargaHoraria() {
+		return cargaHoraria;
+	}
+
+	public void setCargaHoraria(Integer cargaHoraria) {
+		this.cargaHoraria = cargaHoraria;
+	}
+
+	@Override
+	public String toString() {
+		return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao() +", cargaHoraria=" + cargaHoraria + "]";
+	}
+	
+	@Override
+	public double calcularXp() {
+		return XP_PADRAO * cargaHoraria;
+	}
+}
